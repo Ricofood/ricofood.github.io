be6e56621151d79660e67bc740c1ca10dcbc81a5
Ricofood <149512079+Ricofood@users.noreply.github.com>
2023-10-31 12:29:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const contentPanel = document.getElementById("content-panel");
+document.addEventListener("DOMContentLoaded", function () {
+    // Función para cambiar el contenido al cargar la página
+    function mostrarContenido1() {
+        contentPanel.innerHTML = "<p>Contenido 1: Información importante</p>";
+    }
+
+    // Agregar evento de clic al botón 1
+    button1.addEventListener("click", mostrarContenido1);
+
+    // Simular clic en el botón 1 al cargar la página
+    mostrarContenido1();
+});
 
 // Funciones para cambiar el contenido
 function mostrarContenido1() {
